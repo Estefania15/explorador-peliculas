b89c3e27a15871982ae64ec0b8d1b073592c6ea4
Estefania15 <70493414+Estefania15@users.noreply.github.com>
2025-08-15 20:42:02 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, ... } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,5 @@
-const API_KEY = import.meta.env.VITE_OMDB_API_KEY; 
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
 
 export async function searchMovies(query) {
     const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
